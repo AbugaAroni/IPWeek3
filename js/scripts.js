6fed85e439375eaaaecbbd3bdd85f1568255c612
AbugaAroni <abugaaroni@gmail.com>
2020-07-27 02:05:53 +0300

    add css IDs for what we do and jquery code

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $(".clickabledesign").click(function() {
+    $("#design").toggle();
+    $("#hidedesign").toggle(); });
+
+  $(".clickabledevelopment").click(function() {
+    $("#development").toggle();
+    $("#hidedevelopment").toggle(); });
+
+  $(".clickablepmanagement").click(function() {
+    $("#pmanagement").toggle();
+    $("#hidepmanagement").toggle(); });
+ });
