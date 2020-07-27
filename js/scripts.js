0748e4c3ae00bb1602843e22837d26dacad9650d
AbugaAroni <abugaaroni@gmail.com>
2020-07-27 03:19:54 +0300

    add hover and translucent white boxes

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,48 @@
 $(document).ready(function() {
   $(".clickabledesign").click(function() {
     $("#design").toggle();
-    $("#hidedesign").toggle(); });
+    $("#hidedesign").toggle();
+  });
 
   $(".clickabledevelopment").click(function() {
     $("#development").toggle();
-    $("#hidedevelopment").toggle(); });
+    $("#hidedevelopment").toggle();
+  });
 
   $(".clickablepmanagement").click(function() {
     $("#pmanagement").toggle();
-    $("#hidepmanagement").toggle(); });
+    $("#hidepmanagement").toggle();
+  });
+
+  $("#hover1").hover(function(){
+    $(".box1").toggle();
+  });
+
+  $("#hover2").hover(function(){
+    $(".box2").toggle();
+  });
+
+  $("#hover3").hover(function(){
+    $(".box3").toggle();
+  });
+
+  $("#hover4").hover(function(){
+    $(".box4").toggle();
+  });
+
+  $("#hover5").hover(function(){
+    $(".box5").toggle();
+  });
+
+  $("#hover6").hover(function(){
+    $(".box6").toggle();
+  });
+
+  $("#hover7").hover(function(){
+    $(".box7").toggle();
+  });
+
+  $("#hover8").hover(function(){
+    $(".box8").toggle();
+  });
  });
